d9a531c5323d747a1ac40f07b2422c8824d638e2
UchinoKoki <hw21a023@oecu.jp>
2023-12-23 17:13:22 +0900

    [Add]作業途中

--- Assets/Private/Infill/Script/Player/ChangePlayerImage.cs
+++ Assets/Private/Infill/Script/Player/ChangePlayerImage.cs
@@ -2,17 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using BaseDirection;
+using UnityEngine.UI;
 
 /// <summary>
 /// 方向転換したらキャラクターのアニメーションのスイッチを切り替える
 /// </summary>
 public class ChangePlayerImage : MonoBehaviour
 {
-    [SerializeField] private Animator animator;     //動かすAnimator
+    [SerializeField] private Image rightPlayer;
+    [SerializeField] private Image leftPlayer;
+    [SerializeField] private Image upPlayer;
+    [SerializeField] private Image downPlayer;
     private void Start()
     {
-        //もしアタッチがない場合、アタッチされているオブジェクトから取得する
-        if (animator == null) animator = GetComponent<Animator>();
+
     }
     
     /// <summary>
@@ -21,9 +24,9 @@ public class ChangePlayerImage : MonoBehaviour
     /// <param name="direction"></param>
     public void ChangePlayerAnimation(Direction direction)
     {
-        animator.SetBool("isRight",Direction.Right == direction);
-        animator.SetBool("isLeft",Direction.Left == direction);
-        animator.SetBool("isUp",Direction.Up == direction);
-        animator.SetBool("isDown",Direction.Down == direction);
+        rightPlayer.gameObject.SetActive(Direction.Right == direction);
+        leftPlayer.gameObject.SetActive(Direction.Left == direction);
+        upPlayer.gameObject.SetActive(Direction.Up == direction);
+        downPlayer.gameObject.SetActive(Direction.Down == direction);
     }
 }
