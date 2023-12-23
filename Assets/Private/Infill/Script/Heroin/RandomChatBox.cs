e11875142e0f299569ccd412b2aa4ba4dd75d92d
UchinoKoki <hw21a023@oecu.jp>
2023-12-23 20:21:50 +0900

    [Add]吹き出し生成

--- Assets/Private/Infill/Script/Heroin/RandomChatBox.cs
+++ Assets/Private/Infill/Script/Heroin/RandomChatBox.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using BaseDirection;
 
 public enum ChatBoxState
 {
@@ -9,7 +10,7 @@ public enum ChatBoxState
     Move,
     Idle,
     Looking,
-    Repop
+    Repop,
 }
 /// <summary>
 /// 吹きだしの生成と判定を行う
@@ -18,23 +19,29 @@ public class RandomChatBox : MonoBehaviour
 {
     //吹きだし
     [SerializeField] private GameObject rightChatBoxParent;
-    [SerializeField] private GameObject leftChatBoxparent;
+    [SerializeField] private GameObject leftChatBoxParent;
+    [SerializeField] private GameObject upChatBoxParent;
     private List<Image> rightChatBox = new List<Image>();
     private List<Image> leftChatBox = new List<Image>();
+    private List<Image> upChatBox = new List<Image>();
     [SerializeField]private Animator rightAnimator;
     [SerializeField]private Animator leftAnimator;
+    [SerializeField]private Animator upAnimator;
 
     //現在の表示吹きだしの状態
-    [SerializeField,Header("DebugOpen")]private ChatBoxState chatBoxState;
-
+    public ChatBoxState chatBoxState;
+    private Direction chatBoxDirection;
+    private GameObject nowActiveChatBox;
+    private int topChatBox = 0;
+    List<Image> nowActiveChatBoxList = new List<Image>();
     //吹きだし出現タイマー
     private float boxPopTimer = 0f;
 
     //見てる時間タイマー
-    private float lookingTimer = 0f;
+    public float lookingTimer = 0f;
 
     //見てない時間タイマー
-    private float nonLookingTimer = 0f;
+    [SerializeField]private float nonLookingTimer = 0f;
 
     //ランダム生成
     private int boxPosition;
@@ -46,17 +53,25 @@ public class RandomChatBox : MonoBehaviour
     [SerializeField,Header("Param")] private float PopTime = 5f;
     [SerializeField] private float lookingTime = 1f;
     [SerializeField] private float nonLookingTime = 5f;
+
+    //見ている方向取得
+    [SerializeField,Header("Script")] private PlayerLook playerLook;
+
+
     private void Start() {
         //初期化
         chatBoxState = ChatBoxState.NotActive;
+        chatBoxDirection = Direction.Idle;
         //リスト追加
         foreach (Transform child in rightChatBoxParent.transform) rightChatBox.Add(child.GetComponent<Image>());
-        foreach (Transform child in leftChatBoxparent.transform) leftChatBox.Add(child.GetComponent<Image>());
+        foreach (Transform child in leftChatBoxParent.transform) leftChatBox.Add(child.GetComponent<Image>());
+        foreach (Transform child in upChatBoxParent.transform) upChatBox.Add(child.GetComponent<Image>());
         // rightAnimator = rightChatBoxParent.GetComponent<Animator>();
         // leftAnimator = leftChatBoxparent.GetComponent<Animator>();
         //吹きだしの無効化
         SetActiveForListAll(rightChatBox, false);
         SetActiveForListAll(leftChatBox, false);
+        SetActiveForListAll(upChatBox, false);
     }
     private void FixedUpdate() {
         //タイマー調整
@@ -64,33 +79,68 @@ public class RandomChatBox : MonoBehaviour
 
         //タイマー追加
         if(chatBoxState == ChatBoxState.NotActive)boxPopTimer += Time.deltaTime;
-        if(chatBoxState != ChatBoxState.NotActive && chatBoxState != ChatBoxState.Looking) nonLookingTime += Time.deltaTime;
-        if(chatBoxState == ChatBoxState.Looking) lookingTime += Time.deltaTime;
+        if (chatBoxState != ChatBoxState.NotActive && chatBoxState != ChatBoxState.Looking) nonLookingTimer += Time.deltaTime;
+        else nonLookingTimer = 0;
+        if(chatBoxState == ChatBoxState.Looking) lookingTimer += Time.deltaTime;
 
         //処理
         //出現タイマー
-        if(boxPopTimer > PopTime && chatBoxState != ChatBoxState.NotActive)
+        if(boxPopTimer > PopTime && chatBoxState == ChatBoxState.NotActive)
         {
+            boxPopTimer = 0f;
             boxPosition = Random.Range(0, 3);
-
-            //まだ３つ目が完成してないためオーバー
-            if (boxPosition == 2) return;
 
             switch(boxPosition)
             {
                 //Left
                 case 0:
-                    StartChatBox(leftChatBox);
+                    StartChatBox(leftChatBoxParent,leftChatBox);
+                    chatBoxDirection = Direction.Left;
                     break;
                 //Right
                 case 1:
-                    StartChatBox(rightChatBox);
+                    StartChatBox(rightChatBoxParent,rightChatBox);
+                    chatBoxDirection = Direction.Right;
+                    break;
+                case 2:
+                    chatBoxDirection = Direction.Up;
+                    StartChatBox(upChatBoxParent,upChatBox);
                     break;
                 //Error
                 default:
                     Debug.LogError("Random is Over");
                     break;
             }
+        }
+        //今プレイヤーが見ている方向と吹き出し方向の確認
+        if (chatBoxDirection == playerLook.GetDirection())
+        {
+            chatBoxState = ChatBoxState.Looking;
+        }
+        //吹き出し削除
+        if (lookingTimer > lookingTime && chatBoxState == ChatBoxState.Looking)
+        {
+            lookingTimer = 0f;
+            //吹き出し削除
+            nowActiveChatBoxList[topChatBox].gameObject.SetActive(false);
+            topChatBox++;
+            //もし吹き出しが全部消せたら
+            Debug.Log($"topChatbox:{topChatBox},nowActiveChatBoxList{nowActiveChatBoxList.Count}");
+            if(topChatBox == nowActiveChatBoxList.Count)
+            {
+                RemoveChatBox();
+            }
+            //もし吹きだし削除中に別方向を向いたら
+            if(chatBoxDirection != playerLook.GetDirection() && chatBoxDirection != Direction.Idle)
+            {
+                chatBoxState = ChatBoxState.Repop;
+                RepopChatBox(nowActiveChatBox, nowActiveChatBoxList);
+            }
+        }
+        //吹き出しを放置した時
+        if(nonLookingTimer > nonLookingTime)
+        {
+            //茶化しエンド
         }
     }
     ///<summary>
@@ -104,29 +154,42 @@ public class RandomChatBox : MonoBehaviour
     ///もしタイマーがオーバーしたら、茶化しエンドへ強制突入
     ///</summary>
 
-
-    // void CreateChatBox()
-    // {
-    //     GameObject createBox = Instantiate(chatBox[0].gameObject, canvas.transform.position, Quaternion.identity);
-    //     createBox.transform.parent = canvas.transform;
-    // }
-
     ///<summary>
     ///ChatBoxによる妨害を開始する
     ///</sumamry>
-    private void StartChatBox(List<Image> activeList)
+    private void StartChatBox(GameObject activeObjectParent,List<Image> activeList)
     {
+        Debug.Log("Start");
         chatBoxState = ChatBoxState.Move;
+        nowActiveChatBox = activeObjectParent;
+        nowActiveChatBoxList = activeList;
         SetActiveForListAll(activeList, true);
     }
     private void SetActiveForListAll(List<Image> list, bool setBool)
     {
+        Debug.Log("ResetList");
         foreach (Image image in list) image.gameObject.SetActive(setBool);
-        chatBoxState = ChatBoxState.Idle;
+        if(setBool)
+        {
+            chatBoxState = ChatBoxState.Idle;
+        }
     }
-    private void RepopChatBox(List<Image> activeList)
+    private void RepopChatBox(GameObject activeObject,List<Image> activeList)
     {
+        Debug.Log("Repop");
         chatBoxState = ChatBoxState.Repop;
-        StartChatBox(activeList);
+        topChatBox = 0;
+        StartChatBox(activeObject,activeList);
+    }
+    private void RemoveChatBox()
+    {
+        Debug.Log("Remove");
+        topChatBox = 0;
+        SetActiveForListAll(nowActiveChatBoxList, false);
+        chatBoxState = ChatBoxState.NotActive;
+        chatBoxDirection = Direction.Idle;
+
+        //現在の情報初期化
+        topChatBox = 0;
     }
 }
