d7173aa9d37bf0b44c86b4a83664c77c8fc3d560
UchinoKoki <hw21a023@oecu.jp>
2023-12-24 00:36:00 +0900

    [Add]見続けると顔面変更を実装

--- Assets/Private/Infill/Script/Player/ChangePlayerImage.cs
+++ Assets/Private/Infill/Script/Player/ChangePlayerImage.cs
@@ -9,26 +9,38 @@ using UnityEngine.UI;
 /// </summary>
 public class ChangePlayerImage : MonoBehaviour
 {
-    [SerializeField] private GameObject rightPlayer;
+    //見続けてたら顔面が変わる
+    [SerializeField] private float thresholdLookTime = 5f;
+    private float timer;
+    Direction nowDirection;
+    //プレイヤーのオブジェクト
+    [SerializeField,Header("Player")] private GameObject rightPlayer;
     [SerializeField] private GameObject leftPlayer;
     [SerializeField] private GameObject upPlayer;
     [SerializeField] private GameObject downPlayer;
+    [SerializeField] private GameObject changeDownPlayer;//こっちがきもい方
     [SerializeField] private GameObject defaultPlayer;
-    private void Start()
-    {
-
-    }
     
     /// <summary>
     /// プレイヤーアニメーションのスイッチを切り替える
     /// </summary>
     /// <param name="direction"></param>
+    private void FixedUpdate() {
+        if (nowDirection == Direction.Down) timer += Time.deltaTime;
+        if (timer > thresholdLookTime)
+        {
+            timer = 0;
+            downPlayer.SetActive(false);
+            changeDownPlayer.SetActive(true);
+        }
+    }
     public void ChangePlayerAnimation(Direction direction)
     {
         rightPlayer.SetActive(Direction.Right == direction);
         leftPlayer.SetActive(Direction.Left == direction);
         upPlayer.SetActive(Direction.Up == direction);
         downPlayer.SetActive(Direction.Down == direction);
+        changeDownPlayer.SetActive(false);
 
         defaultPlayer.SetActive(false);
     }
